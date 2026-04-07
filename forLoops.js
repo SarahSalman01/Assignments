70ea2359f7dcece22f3c10e9d2d351f1f05d613d
SarahSalman01 <sarahsalman261@gmail.com>
2026-04-07 19:38:05 +0500

    initial commit

---
+++ forLoops.js
@@ -0,0 +1,34 @@
+//FOR LOOP TASKS
+
+// Print numbers from 1 to 10 using a for loop
+for(let i=1; i<=10; i++){
+    console.log(i)
+}
+
+
+// Print all even numbers from 1 to 20
+for (let i= 2; i <=20 ; i+=2) {
+    console.log(i);
+}
+
+
+//Write a program to display the multiplication table of 5
+
+for(let i=1; i<=10; i++){ 
+    console.log("5 * " , i, " = " , i * 5)
+}
+
+
+//Calculate the sum of numbers from 1 to 50
+let sum= 0;
+for(let i=1; i<=50; i++){
+    sum+=i;
+}
+ console.log(sum)
+
+
+//Print numbers in reverse order (10 to 1)
+for(let i=10; i>=1; i--)
+{
+    console.log(i)
+}
---
+++ whileLoop.js
@@ -0,0 +1,37 @@
+//WHILE LOOP TASKS
+
+//Print numbers from 1 to 10 using a while loop
+
+let i=1;
+while (i<=10) {
+    console.log(i);
+    i++;
+}
+
+
+//Print all odd numbers from 1 to 20
+
+let j=1;
+while (j<=20) {
+    console.log(j);
+    j+=2;
+}
+
+
+//Calculate the sum of numbers from 1 to 100
+let sum = 0;
+let k= 1;
+while (i<=100) {
+    sum+=k;
+    k++;
+}
+console.log(sum);
+
+
+//Print numbers in reverse order (10 to 1)
+let n = 10;
+while(n>0){
+    console.log(n);
+    n--;
+}
+
